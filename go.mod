7a417a594542d7aaf4229c3b3a2916af488250c4
ankithans <ankithans1947@gmail.com>
2021-08-06 23:55:16 +0530

    feat: get videos from youtube

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.16.0
+	github.com/joho/godotenv v1.3.0 // indirect
 	google.golang.org/api v0.52.0 // indirect
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.12
--- pkg/routes/yt.go
+++ pkg/routes/yt.go
@@ -1,14 +1,55 @@
 package routes
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/ankithans/youtube-api/pkg/database"
+	"github.com/ankithans/youtube-api/pkg/models"
+	"github.com/ankithans/youtube-api/pkg/utils"
 	"github.com/gofiber/fiber/v2"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 func PostVideos(c *fiber.Ctx) error {
 
 	// Use Youtube API to get videos
+	youtubeService, err := youtube.NewService(
+		context.Background(), option.WithAPIKey(utils.GoDotEnvVariable("YOUTUBE_API_KEY")),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// tim := time.Now().Format(time.RFC3339)
+	res, err := youtubeService.Search.List([]string{"id", "snippet"}).
+		Q("football").
+		MaxResults(10000).
+		// PublishedAfter(tim).
+		Do()
+
+	if err != nil {
+		panic(err)
+	}
+
+	videos := []models.Video{}
+
+	for _, item := range res.Items {
+		fmt.Println(item)
+		if item.Id.Kind == "youtube#video" {
+			vid := models.Video{
+				Title:       item.Snippet.Title,
+				Description: item.Snippet.Description,
+				Date:        item.Snippet.PublishedAt,
+				Thumbnail:   item.Snippet.Thumbnails.Default.Url,
+			}
+			videos = append(videos, vid)
+		}
+	}
 
 	// Store them in database
+	database.DBConn.Create(&videos)
 
-	return c.JSON("hi")
+	return c.JSON(videos)
 }
