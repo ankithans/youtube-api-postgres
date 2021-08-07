197565de299c7dc5dcf29c60037bea5676746405
ankithans <ankithans1947@gmail.com>
2021-08-07 12:41:39 +0530

    feat: add ID as primary key

--- pkg/models/video.go
+++ pkg/models/video.go
@@ -3,6 +3,7 @@ package models
 // Video defines the structure of a video
 // object in the database
 type Video struct {
+	ID          uint   `gorm:"primaryKey"`
 	Title       string `json:"Title"`
 	Description string `json:"Description"`
 	Date        string `json:"Date"`
